15c8d69bb9c128339a2b364c21830a30322b7a9b
Carlos López Bartolomé <calopezb@redhat.com>
2024-07-17 09:48:36 +0200

    Included allow CORS

--- app.py
+++ app.py
@@ -3,12 +3,16 @@ from flask_sqlalchemy import SQLAlchemy
 from config import Config
 from services.analytics_service import get_task_statistics
 from dotenv import load_dotenv
+from flask_cors import CORS
 
 load_dotenv()
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
+
+# Enable CORS for all routes
+CORS(app)
 
 @app.route('/stats', methods=['GET'])
 def stats():
